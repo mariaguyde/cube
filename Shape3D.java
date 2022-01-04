dc019cecebeecee60b3d49ea598332e1d46655f3
Maria guy de fontgalland <90463820+mariaguyde@users.noreply.github.com>
2022-01-04 21:00:52 +0100

    Update Projet  3 Cubes tournants

---
+++ Shape3D.java
@@ -0,0 +1,24 @@
+package cube;
+
+import com.jogamp.opengl.GL2;
+
+public class Shape3D {
+	protected float x, y, z;
+	public float size;
+	public float angle; 
+	
+	public Shape3D()
+	{ 
+		x = 0; 
+		y = 0; 
+		z = 0; 
+		size = 1;
+		angle = 0; 
+	}
+	
+	public void display(GL2 gl, float angle) {
+	
+		
+	}
+
+}
